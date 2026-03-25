adb6da2c60bd245e4b779c86babb9a7ac26d83cf
LevanMarkozia <23202242@ibsu.edu.ge>
2026-03-25 12:50:46 +0400

    Revert "init-project"
    This reverts commit 860b0a9d92554873ff34f313ab099855c0768d16.

--- src/main/java/ge/lmibsu/demo/dto/TestPersonInfo.java
+++
@@ -1,7 +0,0 @@
-package ge.lmibsu.demo.dto;
-
-public record TestPersonInfo(String firstName,String lastName,int age){
-    public TestPersonInfo withAge(int age){
-        return new TestPersonInfo(this.firstName,this.lastName,age);
-    }
-}
